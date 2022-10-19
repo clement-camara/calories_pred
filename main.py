effa882104752009cf99513b08269f17daea8c15
clement <55285912+clement-camara@users.noreply.github.com>
2022-10-19 19:35:37 +0200

    fixe bug

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def app_page(st, **state):
     #### FONCTION POUR LA PREIDTION ET CREATION DU BOUTON AVEC UNE ANIMATION
 
     def predict_(data):
-        with open('/Users/marinelafargue/Desktop/projet calorie/LAST_RUN/Random_forest6_features_pkl', 'rb') as f:
+        with open('/Random_forest6_features_pkl', 'rb') as f:
             model_reg = pickle.load(f)
             return model_reg.predict(data)
 
